f08935d23baad8d4c48525231e6eb9d459075093
EliceVice <eliceios@icloud.com>
2023-08-17 02:49:44 +0200

    Project created

---
+++ GitAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitAppTest
+//
+//  Created by Andrei Isayenka on 17/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
